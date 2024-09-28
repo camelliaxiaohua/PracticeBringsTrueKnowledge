c305138d61bc1760d3b2f3262a7f2d66162d2b29
Camellia.xiaohua <2421195723@qq.com>
2024-09-28 17:12:46 +0800

    SFTP工具类，进行连接和关闭。

---
+++ CloudStorage/src/main/java/camellia/cloudstorage/utils/SftpUtils.java
@@ -0,0 +1,54 @@
+package camellia.cloudstorage.utils;
+
+import camellia.cloudstorage.enums.SftpConfig;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Datetime: 2024/9/28下午1:48
+ * @author: Camellia.xioahua
+ */
+@Slf4j
+public class SftpUtils {
+
+    private static ThreadLocal<ChannelSftp> sftpChannel = new ThreadLocal<>();
+    private static ThreadLocal<Session> session = new ThreadLocal<>();
+
+    private SftpUtils() {}
+
+    public static ChannelSftp openClient() {
+        String userName = SftpConfig.USERNAME.getValue();
+        String host = SftpConfig.HOST.getValue();
+        int port = Integer.parseInt(SftpConfig.PORT.getValue());
+        String password = SftpConfig.PASSWORD.getValue();
+        try {
+            JSch jsch = new JSch();
+            Session sess = jsch.getSession(userName, host, port);
+            sess.setPassword(password);
+            sess.setConfig("StrictHostKeyChecking", "no");
+            sess.connect();
+            ChannelSftp channel = (ChannelSftp) sess.openChannel("sftp");
+            channel.connect();
+            session.set(sess);
+            sftpChannel.set(channel);
+            log.info("连接成功");
+            return channel;
+        } catch (JSchException e) {
+            log.error("连接失败", e);
+            throw new RuntimeException("SFTP连接失败", e);
+        }
+    }
+
+    public static void closeClient() {
+        ChannelSftp channel = sftpChannel.get();
+        if (channel != null) {
+            channel.disconnect();
+            session.get().disconnect();
+            sftpChannel.remove();
+            session.remove();
+        }
+    }
+}
